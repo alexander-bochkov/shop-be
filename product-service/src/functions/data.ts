c03ce80cf14cd99a45ddfdb3054cef02a90c77f6
Aleksandr Bochkov <aleksandr_bochkov@epam.com>
2024-03-22 21:22:03 +0400

    Complete task 3

---
+++ product-service/src/functions/data.ts
@@ -0,0 +1,40 @@
+import type { Product } from './types';
+
+export const products: Array<Product> = [
+  {
+    description: "Dragon’s Dogma is a single player, narrative driven action-RPG series that challenges the players to choose their own experience – from the appearance of their Arisen, their vocation, their party, how to approach different situations and more. Now, in this long-awaited sequel, the deep, explorable fantasy world of Dragon’s Dogma 2 awaits.",
+    id: 0,
+    price: 51.99,
+    title: "Dragon's Dogma 2",
+  },
+  {
+    description: "Join Aloy as she braves a majestic but dangerous new frontier that holds mysterious new threats. This Complete Edition allows you to enjoy the critically acclaimed Horizon Forbidden West on PC in its entirety with bonus content, including the Burning Shores story expansion that picks up after the main game.",
+    id: 1,
+    price: 49.99,
+    title: "Horizon Forbidden West™ Complete Edition",
+  },
+  {
+    description: "Enlist in the Helldivers and join the fight for freedom across a hostile galaxy in a fast, frantic, and ferocious third-person shooter.",
+    id: 2,
+    price: 39.99,
+    title: "HELLDIVERS™ 2",
+  },
+  {
+    description: "Gather your party and return to the Forgotten Realms in a tale of fellowship and betrayal, sacrifice and survival, and the lure of absolute power.",
+    id: 3,
+    price: 29.99,
+    title: "Baldur's Gate 3",
+  },
+  {
+    description: "EA SPORTS FC™ 24 kicks off a new era of The World's Game. Meet the new brand and the three technologies powering the true-to-football experience.",
+    id: 4,
+    price: 69.99,
+    title: "EA SPORTS FC™ 24",
+  },
+  {
+    description: "Tom Clancy's Rainbow Six® Siege is an elite, realistic, tactical team-based shooter where superior planning and execution triumph. It features 5v5 attack vs. defense gameplay and intense close-quarters combat in destructible environments.",
+    id: 5,
+    price: 10.99,
+    title: "Tom Clancy's Rainbow Six® Siege",
+  },
+];
